266d661d32a6e188749d89c61a76066f78a3b92a
songlinhao <455674848@qq.com>
2018-07-09 23:32:23 +0800

    Initial commit

---
+++ pic_spider.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+
+index = 0
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:47.0) '
+                         'Gecko/20100101 Firefox/47.0'}
+
+
+def save_pics(res_url):
+    global index
+    html = BeautifulSoup(requests.get(res_url, headers=headers).text, "lxml")
+    for atext in html.find_all('a', {'class': 'alink'}):
+        imgUrl = atext.get("href")
+        imgUrlHtml = BeautifulSoup(requests.get(imgUrl, headers=headers).text, "lxml")
+        realUrl = imgUrlHtml.find("img").get("src")
+        print(realUrl)
+        with open("images/" + str(index) + ".jpg", "wb") as jpg:
+            jpg.write(requests.get(realUrl).content)
+        index += 1
+
+
+if __name__ == '__main__':
+    url = "http://588ku.com/moban/0-pxnum-1-11-0-0-0-0-1/";
+    for i in range(0, 5):
+        save_pics(url)
+        url = BeautifulSoup(requests.get(url, headers=headers).text, "lxml").find("a", {"class": "downPage"}).get(
+            "href")
